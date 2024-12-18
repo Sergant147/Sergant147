8554d76181ec75830176004cc3452ff07d5041e0
Sergant147 <128633718+Sergant147@users.noreply.github.com>
2024-12-18 22:06:38 +0300

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,6 +1,4 @@
-from datetime import datetime as dt
-import os
-
+history = {}
 
 def show_products(products):
     for i in products:
@@ -16,29 +14,28 @@ def remove_product(products, product):
 
 
 def generate_id():
-    return dt.strftime(dt.now(), '%Y-%m-%d %H-%M-%S')
+    return len(history)+1
 
 
 def make_order(products):
     order_id = generate_id()
-    with open(f'orders/{order_id}.txt', 'w', encoding='UTF-8') as f:
-        for idx, prod in enumerate(products):
-            if idx != len(products) - 1:
-                f.write(f'{prod}\n')
-            else:
-                f.write(f'{prod}')
+    text = ''
+    for idx, prod in enumerate(products):
+        if idx != len(products) - 1:
+            text += f'{prod}\n'
+        else:
+            text += f'{prod}'
+    history[order_id] = text
 
 
 def get_orders_ids():
-    return [path.removesuffix('.txt')
-            for path in os.listdir('orders/')]
+    return list(history.keys())
 
 
 def read_order(order_id):
-    with open(f'orders/{order_id}.txt', 'r', encoding='UTF-8') as f:
-        products = f.readlines()
-        for index in range(len(products)):
-            products[index] = products[index].replace('\n', '')
+    products = []
+    for product in history[order_id].split('\n'):
+        products.append(product)
     return products
 
 
@@ -58,17 +55,20 @@ def show_order(order_id):
     for item_idx, item in enumerate(read_order(order_id)):
         print(f'Item {item_idx + 1}: {item}')
 
+def print_menu():
+    print('1. Показать список продуктов')
+    print('2. Добавить продукт в корзину')
+    print('3. Удалить продукт из корзины')
+    print('4. Сформировать заказ из текущей корзины')
+    print('5. Просмотреть историю заказов')
+    print('6. Просмотреть конкретный заказ из истории')
+    print('7. Кончить шоппинг')
+
 products = []
 def run():
     global products
     while True:
-        print('1. Показать список продуктов')
-        print('2. Добавить продукт в корзину')
-        print('3. Удалить продукт из корзины')
-        print('4. Сформировать заказ из текущей корзины')
-        print('5. Просмотреть историю заказов')
-        print('6. Просмотреть конкретный заказ из истории')
-        print('7. Кончить шоппинг')
+        print_menu()
         try:
             choice = int(input('Выберите пункт меню (1-7): '))
 
@@ -91,7 +91,7 @@ def run():
                 see_orders()
 
             elif choice == 6:
-                order_id = input('ID заказа: ')
+                order_id = int(input('ID заказа: '))
                 show_order(order_id)
 
             elif choice == 7:
